d8472ba19b4dc7d9471c7a57f8d1b46d9602d817
summerva <polinapolina0621@gmail.com>
2026-02-17 21:51:34 +0300

    layout index

---
+++ src/App.jsx
@@ -0,0 +1,148 @@
+import profile from "./assets/profile.png";
+import cart from "./assets/cart.png";
+
+const App = () => {
+  return (
+    <>
+      <div className="header">
+        <h1>OnlineStore</h1>
+        <div className="header__search search">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Введите название..."
+          />
+          <button className="button" type="submit">
+            Поиск
+          </button>
+        </div>
+        <div className="header__nav">
+          <nav>
+            <a href="/profile">
+              <img src={profile} alt="Профиль" />
+            </a>
+            <a href="/cart">
+              <img src={cart} alt="Корзина" />
+            </a>
+          </nav>
+        </div>
+      </div>
+      <div className="main container">
+        <div className="main__sort">
+          <p>Сортировать</p>
+          <select name="sort" id="sort">
+            <option value="popular">Популярные</option>
+            <option value="price-asc">Сначала дешевые</option>
+            <option value="price-desc">Сначала дорогие</option>
+          </select>
+        </div>
+        <div className="main__inner">
+          <div className="main__filter">
+            <ul className="main__block">
+              <p>Категория</p>
+              <li className="main__item">
+                <input type="checkbox" />
+                <label htmlFor="phone">Телефон</label>
+              </li>
+              <li className="main__item">
+                <input type="checkbox" />
+                <label htmlFor="phone">Телефон</label>
+              </li>
+            </ul>
+            <div className="search__block main__block">
+              <div className="search">
+                <p>Бренд</p>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  placeholder="Поиск значений"
+                />
+              </div>
+              <ul className="main__block">
+                <li className="main__item">
+                  <input type="checkbox" />
+                  <label htmlFor="phone">Телефон</label>
+                </li>
+                <li className="main__item">
+                  <input type="checkbox" />
+                  <label htmlFor="phone">Телефон</label>
+                </li>
+              </ul>
+            </div>
+            <div className="price__block main__block">
+              <p>Цена</p>
+              <div className="price__block">
+                <div className="search">
+                  <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="0"
+                  />
+                </div>
+                <div className="search">
+                  <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="99999"
+                  />
+                </div>
+              </div>
+              <ul className="main__block">
+                <li className="main__item">
+                  <input type="checkbox" />
+                  <label htmlFor="phone">До 25000</label>
+                </li>
+                <li className="main__item">
+                  <input type="checkbox" />
+                  <label htmlFor="phone">До 50000</label>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="main__grid">
+            <div className="grid__products">
+              <div className="product__card">
+                <img src="https://img.mvideo.ru/Big/400454546bb1.jpg" alt="" />
+                <div className="product__inner">
+                  <h3>Название</h3>
+                  <p>Цена</p>
+                </div>
+                <button className="product_button button">Купить</button>
+              </div>
+              <div className="product__card">
+                <img src="https://img.mvideo.ru/Big/400454546bb1.jpg" alt="" />
+                <div className="product__inner">
+                  <h3>Название</h3>
+                  <p>Цена</p>
+                </div>
+                <button className="product_button button">Купить</button>
+              </div>
+              <div className="product__card">
+                <img src="https://img.mvideo.ru/Big/400454546bb1.jpg" alt="" />
+                <div className="product__inner">
+                  <h3>Название</h3>
+                  <p>Цена</p>
+                </div>
+                <button className="product_button button">Купить</button>
+              </div>
+              <div className="product__card">
+                <img src="https://img.mvideo.ru/Big/400454546bb1.jpg" alt="" />
+                <div className="product__inner">
+                  <h3>Название</h3>
+                  <p>Цена</p>
+                </div>
+                <button className="product_button button">Купить</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
